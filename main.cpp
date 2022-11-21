c133b0757bbf07c73300c9eca4760c0b21b869d7
NicolasUrban2 <nclsurban@gmail.com>
2022-11-21 17:08:48 +0100

    Ajout de la fonction chercherTitre

--- main.cpp
+++ main.cpp
@@ -2,16 +2,35 @@
 #include <fstream>
 #include <stdlib.h>
 #include <cstring>
+#include <string>
+#include <sstream>
 
 using namespace std;
 
 void convertirPDF(string file);
 string recupererText(string file);
+string chercherTitre(string texte);
 
 int main(int argvc, char *argv[])
 {
+	string texte;
+	texte = recupererText("CORPUS_TRAIN/hybrid_approach");
+	string titre;
+	titre = chercherTitre(texte);
+	cout<<titre<<endl;
 	return 0;
 }
+
+/**
+ * 	Récupère la première ligne de "texte" (qui représente en général le titre)
+ */
+
+string chercherTitre(string texte)
+{
+	string titre;
+	titre = texte.substr(0, texte.find("\n"));
+	return titre;
+} 
 
 /**
  * 	Récupère le contenu du PDF file (sans l'extension .pdf) et le retourne sous forme d'un string
