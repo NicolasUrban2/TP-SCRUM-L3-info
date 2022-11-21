ef7e374a290fc93d8b16c9401d64dc36d6c4ff8c
NicolasUrban2 <nclsurban@gmail.com>
2022-11-21 18:40:54 +0100

    Ajout de la fonction parser

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 #include <cstring>
 #include <string>
-#include <sstream>
+#include <sys/types.h>
+#include <dirent.h>
+#include <direct.h>
 
 using namespace std;
 
@@ -11,22 +13,69 @@ void convertirPDF(string file);
 string recupererText(string file);
 string chercherTitre(string texte);
 string chercherAbstract(string texte);
+void parser(string directory);
 
 int main(int argvc, char *argv[])
 {
-	string texte;
-	texte = recupererText("CORPUS_TRAIN/ACL2004-HEADLINE");
-	string titre;
-	titre = chercherTitre(texte);
-	cout<<titre<<endl;
-	string abstract;
-	abstract = chercherAbstract(texte);
-	cout<<abstract<<endl;
+	parser("CORPUS_TRAIN/");
 	return 0;
 }
 
 /**
- * 	Récupère la ligne de "texte" en dessous du premier mot "abstract"
+ * 	Récupère le nom, le titre et le résumé de tous les articles PDF dans le répertoire et les ressort dans des .txt dans un sous répertoire
+ *	[directory] doit avoir un '/' à la fin
+ */ 
+
+void parser(string directory)
+{
+	string currentFile;
+	DIR* dir = opendir(directory.c_str());
+	struct dirent * dp;
+	dp = readdir(dir);
+
+	string removeCommand = "rm -r "+directory+"sorties/";
+	string commande = directory+"sorties/";
+	system(removeCommand.c_str());	// Supprime le sous répertoire sorties/ s'il existe déjà (n'est pas bloquant sinon)
+	//cerr<<removeCommand<<endl;
+	_mkdir(commande.c_str());	// Crée le sous répertoire sorties/ (ne fonctionnait pas avec une commande mkdir dans system())
+	//cerr<<commande.c_str()<<endl;
+	
+	// Pour chaque fichier dans le répertoire directory
+	while(dp)
+	{
+		currentFile = dp->d_name;
+		size_t extension = currentFile.find(".pdf");
+		if(extension!=string::npos)		// Traite les fichiers .pdf uniquement
+		{
+			currentFile = currentFile.substr(0, currentFile.find(".pdf"));	// Récupère le nom du fichier sans l'extension
+			string texte;
+			texte = recupererText(directory+currentFile);	// Récupère le texte du PDF
+
+			string titre;
+			titre = chercherTitre(texte);	// Récupère le titre du PDF
+
+			string abstract;
+			abstract = chercherAbstract(texte);	// Récupère le résumé du PDF
+
+			// Ecrit les résultats dans un fichier .txt du même nom que le PDF dans le sous répertoire sorties/
+			string txtPath = directory+"sorties/"+currentFile+".txt";
+			ofstream flux(txtPath.c_str());
+			if(flux)
+			{
+				flux<<currentFile<<"\n"<<titre<<"\n"<<abstract<<endl;
+			}
+			else
+			{
+				cerr<<"Erreur lors de l'ouverture du fichier "<<txtPath<<endl;
+			}
+		}
+		dp=readdir(dir);
+	}
+	closedir(dir);
+}
+
+/**
+ * 	Récupère la ligne de "texte" en dessous du premier mot "Abstract"
  */ 
 
 string chercherAbstract(string texte)
@@ -71,6 +120,9 @@ string recupererText(string file)
 			content +=ligne;
 			content +="\n";
 		}
+		flux.close();	// Ferme le flux avant de supprimer le fichier (le texte a été récupéré, il faut nettoyer l'environnement)
+		remove(txtFile.c_str());	// Supprime le fichier txt
+		cerr<<txtFile.c_str()<<endl;
 		return content;
 	}
 	return NULL;
