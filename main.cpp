81150f8cb9ad0882f12177d997954995f4eb4915
NicolasUrban2 <nclsurban@gmail.com>
2022-11-21 17:28:23 +0100

    Ajout de la fonction chercherAbstract

--- main.cpp
+++ main.cpp
@@ -10,15 +10,35 @@ using namespace std;
 void convertirPDF(string file);
 string recupererText(string file);
 string chercherTitre(string texte);
+string chercherAbstract(string texte);
 
 int main(int argvc, char *argv[])
 {
 	string texte;
-	texte = recupererText("CORPUS_TRAIN/hybrid_approach");
+	texte = recupererText("CORPUS_TRAIN/ACL2004-HEADLINE");
 	string titre;
 	titre = chercherTitre(texte);
 	cout<<titre<<endl;
+	string abstract;
+	abstract = chercherAbstract(texte);
+	cout<<abstract<<endl;
 	return 0;
+}
+
+/**
+ * 	Récupère la ligne de "texte" en dessous du premier mot "abstract"
+ */ 
+
+string chercherAbstract(string texte)
+{
+	string abstract;
+	size_t firstAbstract = texte.find("Abstract");
+	if(firstAbstract==string::npos)
+	{
+		return "Aucun resume";	
+	}
+	abstract = texte.substr(firstAbstract+9, texte.find("\n",firstAbstract+9)-(firstAbstract+9));
+	return abstract;
 }
 
 /**
